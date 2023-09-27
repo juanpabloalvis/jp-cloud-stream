73bc2bcdcac99386a7c635be22a227c7b9e44542
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-27 11:57:14 -0500

    Commit inicial

---
+++ src/main/java/jpcloudstream/JpCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package jpcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpCloudStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpcloudstream/JpCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package jpcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
